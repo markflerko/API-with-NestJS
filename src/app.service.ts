a631434dd986768fa89c2ab2da4944434531efc3
markflerko <markflerko@gmail.com>
2022-06-03 23:49:40 +0400

    feat: pr commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'не хуита!';
   }
   getHello1(): string {
     return 'Hello World1!';
