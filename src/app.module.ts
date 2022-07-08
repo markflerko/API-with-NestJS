bd3f2fe30053fd204eac454bf3cfb39313f1fc86
markflerko <markflerko@gmail.com>
2022-07-08 16:29:01 +0400

    feat: 08 unit tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { CategoryModule } from './category/category.module';
 import { DatabaseModule } from './database/database.module';
 import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
-import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [PostsModule, DatabaseModule, UsersModule, AuthenticationModule, CategoryModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PostsModule,
+    DatabaseModule,
+    UsersModule,
+    AuthenticationModule,
+    CategoryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/authentication/tests/authentication.service.spec.ts
@@ -0,0 +1,42 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import User from '../../users/user.entity';
+import { UsersService } from '../../users/users.service';
+import mockedConfigService from '../../utils/mocks/config.service';
+import mockedJwtService from '../../utils/mocks/jwt.service';
+import { AuthenticationService } from '../authentication.service';
+
+describe('The AuthenticationService', () => {
+  let authenticationService: AuthenticationService;
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        AuthenticationService,
+        {
+          provide: ConfigService,
+          useValue: mockedConfigService,
+        },
+        {
+          provide: JwtService,
+          useValue: mockedJwtService,
+        },
+        {
+          provide: getRepositoryToken(User),
+          useValue: {},
+        },
+      ],
+    }).compile();
+    authenticationService = await module.get(AuthenticationService);
+  });
+  describe('when creating a cookie', () => {
+    it('should return a string', () => {
+      const userId = 1;
+      expect(
+        typeof authenticationService.getCookieWithJwtToken(userId),
+      ).toEqual('string');
+    });
+  });
+});
