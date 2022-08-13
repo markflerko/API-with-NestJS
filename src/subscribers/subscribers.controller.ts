47b676652e9c892ea62f53835b51adfcd0dd5407
markflerko <markflerko@gmail.com>
2022-08-14 00:51:18 +0400

    20: gRPC

--- src/subscribers/subscribers.controller.ts
+++ src/subscribers/subscribers.controller.ts
@@ -4,39 +4,36 @@ import {
   Controller,
   Get,
   Inject,
+  OnModuleInit,
   Post,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientGrpc } from '@nestjs/microservices';
 import JwtAuthenticationGuard from '../authentication/jwt-authentication.guard';
 import CreateSubscriberDto from './dto/createSubscriber.dto';
+import SubscribersService from './subscribers.service.interface';
 
 @Controller('subscribers')
 @UseInterceptors(ClassSerializerInterceptor)
-export default class SubscribersController {
-  constructor(
-    @Inject('SUBSCRIBERS_SERVICE') private subscribersService: ClientProxy,
-  ) {}
+export default class SubscribersController implements OnModuleInit {
+  private subscribersService: SubscribersService;
+
+  constructor(@Inject('SUBSCRIBERS_PACKAGE') private client: ClientGrpc) {}
+
+  onModuleInit() {
+    this.subscribersService =
+      this.client.getService<SubscribersService>('SubscribersService');
+  }
 
   @Get()
   async getSubscribers() {
-    return this.subscribersService.send(
-      {
-        cmd: 'get-all-subscribers',
-      },
-      '',
-    );
+    return this.subscribersService.getAllSubscribers({});
   }
 
   @Post()
   @UseGuards(JwtAuthenticationGuard)
   async createPost(@Body() subscriber: CreateSubscriberDto) {
-    return this.subscribersService.send(
-      {
-        cmd: 'add-subscriber',
-      },
-      subscriber,
-    );
+    return this.subscribersService.addSubscriber(subscriber);
   }
 }
--- src/subscribers/subscribers.module.ts
+++ src/subscribers/subscribers.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import SubscribersController from './subscribers.controller';
 
 @Module({
@@ -8,21 +9,14 @@ import SubscribersController from './subscribers.controller';
   controllers: [SubscribersController],
   providers: [
     {
-      provide: 'SUBSCRIBERS_SERVICE',
+      provide: 'SUBSCRIBERS_PACKAGE',
       useFactory: (configService: ConfigService) => {
-        const user = configService.get('RABBITMQ_USER');
-        const password = configService.get('RABBITMQ_PASSWORD');
-        const host = configService.get('RABBITMQ_HOST');
-        const queueName = configService.get('RABBITMQ_QUEUE_NAME');
-
         return ClientProxyFactory.create({
-          transport: Transport.RMQ,
+          transport: Transport.GRPC,
           options: {
-            urls: [`amqp://${user}:${password}@${host}`],
-            queue: queueName,
-            queueOptions: {
-              durable: true,
-            },
+            package: 'subscribers',
+            protoPath: join(process.cwd(), 'src/subscribers/subscribers.proto'),
+            url: configService.get('GRPC_CONNECTION_URL'),
           },
         });
       },
