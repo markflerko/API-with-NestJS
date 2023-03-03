7ca8515cf0e4a239ac6568d08065072f4165cce1
Mark Flerko <markflerko@gmail.com>
2023-03-03 13:43:37 +0300

    init

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -7,15 +7,27 @@ import CategoryNotFoundException from './exceptions/postNotFund.exception';
 
 @Injectable()
 export class CategoryService {
+  /**
+   * @ignore
+   */
   constructor(
     @InjectRepository(Category)
     private categoryService: Repository<Category>,
   ) {}
 
+  /**
+   * @deprecated
+   * A method that fetches the categories from the database
+   * @returns A promise with the list of categories
+   */
   getAllCategories() {
     return this.categoryService.find({ relations: ['posts'] });
   }
 
+  /**
+   * A method that get a category from the database
+   * @param id An id of a category. A category with this id should exist in the database
+   */
   async getCategoryById(id: number) {
     const category = await this.categoryService.findOne({
       where: { id },
@@ -27,6 +39,9 @@ export class CategoryService {
     throw new CategoryNotFoundException(id);
   }
 
+  /**
+   * See the [definition of the UpdateCategoryDto file]{@link UpdateCategoryDto} to see a list of required properties
+   */
   async updateCategory(id: number, category: UpdateCategoryDto) {
     await this.categoryService.update(id, category);
     const updatedCategory = await this.categoryService.findOne({
